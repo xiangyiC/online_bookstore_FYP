5c9ff27c7b07373ef11b871bc998e3efa7c57677
xiangyiC <D200166C@sc.edu.my>
2022-07-01 18:05:44 +0800

    complete admin basic layout

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -27,9 +27,22 @@
             }
 
             $(document).ready(function(){
-                $(".sidebar-dropdown-link").click(function(){
-                    $(".sidebar-submenu-show").toggleClass("sidebar-submenu-show-show");
-                    $(".sidebar-dropdown-link").toggleClass("sidebar-dropdown-link-active");
+                $(".sidebar-dropdown-link-book").click(function(){
+                    $(".sidebar-submenu-show-book").toggleClass("sidebar-submenu-show-show-book");
+                    $(".sidebar-dropdown-link-book").toggleClass("sidebar-dropdown-link-active-book");
+                    $('.sidebar-submenu-show-show-stationery').removeClass('sidebar-submenu-show-show-stationery');
+                    $('.sidebar-dropdown-link-active-stationery').removeClass('sidebar-dropdown-link-active-stationery');
+                });
+                $(".sidebar-dropdown-link-stationery").click(function(){
+                    $(".sidebar-submenu-show-stationery").toggleClass("sidebar-submenu-show-show-stationery");
+                    $(".sidebar-dropdown-link-stationery").toggleClass("sidebar-dropdown-link-active-stationery");
+                    $('.sidebar-submenu-show-show-book').removeClass('sidebar-submenu-show-show-book');
+                    $('.sidebar-dropdown-link-active-book').removeClass('sidebar-dropdown-link-active-book');
+                });
+
+                $(".sidebar-show").click(function(){
+                    $(".sidebar").toggleClass("sidebar-hide");
+                    
                 });
             });
 
@@ -54,27 +67,27 @@
                             </li>
 
                             <li class="sidebar-item sidebar-dropdown">
-                                <a class="sidebar-link sidebar-dropdown-link">
+                                <a class="sidebar-link sidebar-dropdown-link-book">
                                 <i class="bi bi-tag"></i></i> <span class="align-middle">Book</span>
                                     <i class="bi bi-caret-down-fill book-arrow-down"></i>
                                 </a>
                                 
                                 <div class="sidebar-submenu">
-                                    <ul class="sidebar-submenu-show">
+                                    <ul class="sidebar-submenu-show-book">
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-plus-circle"></i> Add Book Category</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-plus-circle"></i> Add Category</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-x-circle"></i> Delete Book Category</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-x-circle"></i> Delete Category</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Edit Book Category</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Edit Category</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Add Book</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-plus-square"></i> Add Book</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Delete Book</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-x-square"></i> Delete Book</a>
                                         </li>
                                         <li>
                                             <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Edit Book</a>
@@ -84,21 +97,21 @@
                             </li>
 
                             <li class="sidebar-item sidebar-dropdown">
-                                <a class="sidebar-link sidebar-dropdown-link">
+                                <a class="sidebar-link sidebar-dropdown-link-stationery">
                                 <i class="bi bi-tag"></i></i> <span class="align-middle">Stationery</span>
                                     <i class="bi bi-caret-down-fill stationery-arrow-down"></i>
                                 </a>
                                 
                                 <div class="sidebar-submenu">
-                                    <ul class="sidebar-submenu-show">
+                                    <ul class="sidebar-submenu-show-stationery">
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-plus-square"></i> Add Stationery Category</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-plus-square"></i> Add Category</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-x-square"></i> Delete Stationery Category</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-x-square"></i> Delete Category</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Edit Stationery Category</a>
+                                            <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Edit Category</a>
                                         </li>
                                         <li>
                                             <a href="#" class="sidebar-link"><i class="bi bi-pencil"></i> Add Stationery</a>
@@ -128,6 +141,44 @@
 
                     </div>
                 </nav>
+
+                <!--Navigation Bar-->
+                <div class="main">
+                    <nav class="navbar navbar-expand navbar-light navbar-bg">
+                        <a class="sidebar-toggle sidebar-show">
+                            <i class="bi bi-list hamburger"></i>
+                        </a>
+
+                        <div class="navbar-collapse collapse">
+                            <ul class="navbar-nav navbar-align">
+                                <li class="nav-item">
+                                    <a class="nav-link home">Home</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle d-none d-sm-inline-block user rounded me-1" href="#" data-bs-toggle="dropdown">
+                                        <img src="{{asset('images/hexiao.jpg')}}" class="avatar img-fluid rounded me-1" alt="user" /> <span class="text-dark username">He Xiao</span>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-end about-user">
+                                        <a class="dropdown-item" href=""><i class="bi bi-person-bounding-box"></i>	Profile</a>
+                                        <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#user-manual"><i class="bi bi-book"></i>	User Mannual</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();"><i class="bi bi-box-arrow-right"></i>{{ __('Logout') }}</a>
+                                            </a>
+
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    @yield('content')
+                </div>
+
+
             </div>    
         </body>
 
